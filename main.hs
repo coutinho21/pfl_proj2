b1bccf95a417db04f1720379aa6e8eceaa67cc2d
xouteiro <xavierouteiro@gmail.com>
2023-12-27 14:27:47 +0000

    only to tests give an error

--- main.hs
+++ main.hs
@@ -172,7 +172,7 @@ compB :: Bexp -> Code
 compB BTrue = [Push 1]
 compB BFalse = [Push 0]
 compB (Eq a1 a2) = compA a1 ++ compA a2 ++ [Equ]
-compB (Leq a1 a2) = compA a1 ++ compA a2 ++ [Le]
+compB (Leq a1 a2) = compA a2 ++ compA a1 ++ [Le]
 compB (Not b) = compB b ++ [Neg]
 compB (And2 b1 b2) = compB b1 ++ compB b2 ++ [And]
 
@@ -285,9 +285,9 @@ testAll2 :: IO [String]
 testAll2 = mapM test2
   [ ("x := 5; x := x - 1;", ("","x=4"))
   , ("i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);", ("","fact=3628800,i=1"))
-  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;", ("","x=2,z=4")) --da mal porque faz o else mesmo fazendo o if
-  , ("x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)", ("","x=1"))  --da mal porque faz o else mesmo fazendo o if
-  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;", ("","x=2"))    --da mal porque faz o else mesmo fazendo o if
-  , ("if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;", ("","y=2")) --da erro
+  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;", ("","x=2,z=4"))
+  , ("x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)", ("","x=1")) 
+  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;", ("","x=2"))   
   , ("x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);", ("","x=2,y=-10,z=6"))   --da erro
+  , ("if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;", ("","y=2")) --da erro
   ]
