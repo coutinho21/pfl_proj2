9a2968a26a11c06b7cb090e3e696e763bfca0346
xouteiro <xavierouteiro@gmail.com>
2023-12-27 01:01:18 +0000

    started ex2, only 2 test passed

--- main.hs
+++ main.hs
@@ -3,6 +3,10 @@
 import Data.List
 import Data.Maybe
 import Control.Exception
+import Text.Read (readMaybe)
+import Data.List.Split (splitOn)
+import Data.List (intercalate)
+
 -- Part 1
 
 -- Do not modify our definition of Inst and Code
@@ -142,24 +146,146 @@ testAll = mapM test
 
 -- -- Part 2
 
--- -- TODO: Define the types Aexp, Bexp, Stm and Program
+-- Arithmetic expressions
+data Aexp = Var String
+          | IntConst Integer
+          | Add2 Aexp Aexp
+          | Sub2 Aexp Aexp
+          | Mul Aexp Aexp
+          | BinaryOp String Aexp Aexp
+          deriving Show
 
--- -- compA :: Aexp -> Code
--- compA = undefined -- TODO
+-- Boolean expressions
+data Bexp = BTrue
+          | BFalse
+          | Eq Aexp Aexp
+          | Leq Aexp Aexp
+          | Not Bexp
+          | And2 Bexp Bexp
+          deriving Show
 
--- -- compB :: Bexp -> Code
--- compB = undefined -- TODO
+-- Statements
+data Stm = Assign String Aexp
+         | Seq [Stm]
+         | If Bexp Stm Stm
+         | While Bexp Stm
+         | Compound Stm Stm
+         deriving Show
 
--- -- compile :: Program -> Code
--- compile = undefined -- TODO
+-- Program
+type Program = [Stm]
 
--- -- parse :: String -> Program
--- parse = undefined -- TODO
+
+compA :: Aexp -> Code
+compA (Var x) = [Fetch x]
+compA (IntConst n) = [Push n]
+compA (Add2 a1 a2) = compA a1 ++ compA a2 ++ [Add]
+compA (Sub2 a1 a2) = compA a1 ++ compA a2 ++ [Sub]
+compA (Mul a1 a2) = compA a1 ++ compA a2 ++ [Mult]
+compA (BinaryOp op a1 a2) = case op of
+  "-" -> compA a2 ++ compA a1 ++ [Sub]
+  _ -> compA a1 ++ compA a2 ++ case op of
+    "+" -> [Add]
+    "*" -> [Mult]
+  
+compB :: Bexp -> Code
+compB BTrue = [Push 1]
+compB BFalse = [Push 0]
+compB (Eq a1 a2) = compA a1 ++ compA a2 ++ [Equ]
+compB (Leq a1 a2) = compA a1 ++ compA a2 ++ [Le]
+compB (Not b) = compB b ++ [Neg]
+compB (And2 b1 b2) = compB b1 ++ compB b2 ++ [And]
+
+compile :: Program -> Code
+compile [] = []
+compile ((Assign x a):ss) = compA a ++ [Store x] ++ compile ss
+compile ((Seq ss):ss') = compile ss ++ compile ss'
+compile ((If b s1 s2):ss) = compB b ++ [Branch (compile [s1]) (compile [s2])] ++ compile ss
+compile ((While b s):ss) = [Loop (compB b) (compile [s])] ++ compile ss
+compile ((Compound s1 s2):ss) = compile [s1] ++ compile [s2] ++ compile ss
+
+
+type Token = String
+
+
+
+
+tokenize :: String -> [Token]
+tokenize = words . concatMap spaceOut . replaceAll
+  where 
+    spaceOut c = if c `elem` "();+-*/"
+                  then [' ', c, ' ']
+                  else [c]
+    replaceAll = replace ":=" ":= " . replace "<=" "<= " . replace ">=" ">= " . replace "==" "== "
+    replace old new = intercalate new . splitOn old
+parseAexp :: [Token] -> (Aexp, [Token])
+parseAexp (t:ts) 
+  | Just n <- readMaybe t = (IntConst n, ts)
+  | otherwise = case ts of
+      (op:a2:ts') | op `elem` ["+", "-", "*", "/"] -> 
+        let (a2', ts'') = parseAexp (a2:ts')
+        in (BinaryOp op (Var t) a2', ts'')
+      _ -> (Var t, ts)
+
+parseBexp :: [Token] -> (Bexp, [Token])
+parseBexp ("not":ts) = let (b, ts') = parseBexp ts in (Not b, ts')
+parseBexp ("(":ts) = 
+  let (b, ")":ts') = parseBexp ts
+  in (b, ts')
+parseBexp ts = 
+  let (a1, op:ts') = parseAexp ts
+      (a2, ts'') = parseAexp ts'
+  in case op of
+       "==" -> (Eq a1 a2, ts'')
+       "<=" -> (Leq a1 a2, ts'')
+       _ -> error $ "Unknown operator: " ++ op
+            
+
+parseStm :: [Token] -> (Stm, [Token])
+parseStm [] = error "Unexpected end of input"
+parseStm ("(":ts) = 
+  let (s, ts') = parseStm ts
+  in case ts' of
+       (";":ts'') -> let (s', ";":ts''') = parseStm ts''
+                     in case ts''' of
+                          (")":ts'''') -> ((Compound s s'), ts'''')
+                          _ -> error $ "Expected ')', got: " ++ show ts'''
+       _ -> error $ "Expected ';', got: " ++ show ts'
+
+parseStm (t:":=":ts) = 
+  let (a, ts') = parseAexp ts
+  in (Assign t a, ts')
+parseStm ("if":ts) = 
+  let (b, "then":ts') = parseBexp ts
+      (s1, ";":ts'') = parseStm ts'
+  in case ts'' of
+       ("else":ts''') -> let (s2, ts'''') = parseStm ts'''
+                         in (If b s1 s2, ts'''')
+       _ -> error $ "Expected 'else', got: " ++ show ts''
+parseStm ("while":ts) = 
+  let (b, ts') = parseBexp ts
+  in case dropWhile (/= "do") ts' of
+       ("do":ts'') -> let (s, ts''') = parseStm ts''
+                      in (While b s, ts''')
+       _ -> error $ "Expected 'do', got: " ++ show ts'
+parseStm ts = error $ "Unexpected tokens: " ++ show ts
+
+parseProgram :: [Token] -> Program
+parseProgram [] = []
+parseProgram ts = 
+  let (s, ts') = parseStm ts
+      ts'' = case ts' of
+        (";":rest) -> rest
+        _ -> ts'
+  in s : parseProgram ts''
+
+parse :: String -> Program
+parse = parseProgram . tokenize
 
 -- -- To help you test your parser
--- testParser :: String -> (String, String)
--- testParser programCode = (stack2Str stack, store2Str store)
---   where (_,stack,store) = run(compile (parse programCode), createEmptyStack, createEmptyStore)
+testParser :: String -> (String, String)
+testParser programCode = (stack2Str stack, state2Str state)
+   where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
 
 -- -- Examples:
 -- -- testParser "x := 5; x := x - 1;" == ("","x=4")
@@ -169,3 +295,26 @@ testAll = mapM test
 -- -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
 -- -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
+
+test2 :: (String, (String, String)) -> IO String
+test2 (code, expected) = 
+  catch 
+    (do
+      let result = testParser code
+      return $ if result == expected then "True" else "False")
+    handler
+  where
+    handler :: SomeException -> IO String
+    handler _ = return "Error"
+
+testAll2 :: IO [String]
+testAll2 = mapM test2
+  [ ("x := 5; x := x - 1;", ("","x=4"))
+  , ("i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);", ("","fact=3628800,i=1"))
+  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;", ("","x=2,z=4")) --da mal porque faz o else mesmo fazendo o if
+  , ("x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)", ("","x=1"))  --da mal porque faz o else mesmo fazendo o if
+  , ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;", ("","x=2"))    --da mal porque faz o else mesmo fazendo o if
+  , ("if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;", ("","y=2")) --da erro
+  , ("x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);", ("","x=2,y=-10,z=6"))   --da erro
+  ]
